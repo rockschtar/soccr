0d539102a9273b50491f5e7bfca57dad4f01c966
Stefan Helmer <rockschtar@gmail.com>
2024-03-10 14:59:36 +0100

    WIP

--- soccr-reloaded.php
+++ soccr-reloaded.php
@@ -13,27 +13,30 @@
  * @package         Rockschtar\WordPress\Soccr
  */
 
-
-use Rockschtar\WordPress\Soccr\Controller\PluginController;
-
 define('SOCCR_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('SOCCR_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-spl_autoload_register(static function ($class) {
-    $baseDir = __DIR__ . '/src/';
-    $prefix = 'Rockschtar\\WordPress\\Soccr\\';
-    $len = strlen($prefix);
+if(file_exists(SOCCR_PLUGIN_DIR . 'vendor/autoload.php')) {
+    require_once SOCCR_PLUGIN_DIR . 'vendor/autoload.php';
+} else {
+    spl_autoload_register(static function ($class) {
+        $baseDir = __DIR__ . '/src/';
+        $prefix = 'Rockschtar\\WordPress\\Soccr\\';
+        $len = strlen($prefix);
 
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
+        if (strncmp($prefix, $class, $len) !== 0) {
+            return;
+        }
 
-    $relativeClass = substr($class, $len);
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+        $relativeClass = substr($class, $len);
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
-    if (file_exists($file)) {
-        include $file;
-    }
-});
+        if (file_exists($file)) {
+            include $file;
+        }
+    });
+}
+
+use Rockschtar\WordPress\Soccr\Controller\PluginController;
 
 PluginController::init();
