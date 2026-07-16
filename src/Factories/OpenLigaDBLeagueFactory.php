c7db91d339afc163c9e38ed1c30a862f4f92a74c
Stefan Helmer <rockschtar@pm.me>
2026-07-16 23:43:23 +0200

    Add short league name handling in OpenLigaDB:
    - Introduced `leagueNameShort` property to the `OpenligaDBLeague` model.
    - Added functionality to generate shortened Bundesliga names in `OpenLigaDBLeagueFactory`.
    - Updated `OpenLigaDBCommand` and serialization logic to include `leagueNameShort`.
    - Implemented unit tests to validate `leagueNameShort` generation for Bundesliga names with "ß" and "ss".

--- src/Factories/OpenLigaDBLeagueFactory.php
+++ src/Factories/OpenLigaDBLeagueFactory.php
@@ -9,13 +9,23 @@ class OpenLigaDBLeagueFactory
     public static function createFromJSON(\stdClass $league): OpenligaDBLeague
     {
         $openLigaDBLeague = new OpenligaDBLeague();
-
         $openLigaDBLeague->setLeagueId($league->leagueId);
         $openLigaDBLeague->setLeagueName($league->leagueName);
+        $openLigaDBLeague->setLeagueNameShort(self::createLeagueNameShort($league->leagueName));
         $openLigaDBLeague->setLeagueShortcut($league->leagueShortcut);
         $openLigaDBLeague->setLeagueSeason((int) $league->leagueSeason);
         $openLigaDBLeague->setSportId((int) $league->sport->sportId);
 
         return $openLigaDBLeague;
+    }
+    private static function createLeagueNameShort(string $leagueName): string
+    {
+        $leagueNameShort = preg_replace(
+            '/^(\d+\.\s*)Fu(?:ß|ss)ball-Bundesliga(\s+\d{4}\/\d{4})$/u',
+            '$1Bundesliga$2',
+            $leagueName,
+        );
+
+        return $leagueNameShort ?? $leagueName;
     }
 }
--- tests/Unit/Factories/OpenLigaDBLeagueFactoryTest.php
+++ tests/Unit/Factories/OpenLigaDBLeagueFactoryTest.php
@@ -26,6 +26,36 @@ class OpenLigaDBLeagueFactoryTest extends UnitTestCase
         $this->assertSame(1, $league->getSportId());
     }
 
+    public function test_shortens_bundesliga_name_with_eszett(): void
+    {
+        $json = (object) [
+            'leagueId'       => 3,
+            'leagueName'     => '1. Fußball-Bundesliga 2024/2025',
+            'leagueShortcut' => 'bl1',
+            'leagueSeason'   => '2024',
+            'sport'          => (object) ['sportId' => '1'],
+        ];
+
+        $league = OpenLigaDBLeagueFactory::createFromJSON($json);
+
+        $this->assertSame('1. Bundesliga 2024/2025', $league->getLeagueNameShort());
+    }
+
+    public function test_shortens_bundesliga_name_with_double_s(): void
+    {
+        $json = (object) [
+            'leagueId'       => 4,
+            'leagueName'     => '1. Fussball-Bundesliga 2024/2025',
+            'leagueShortcut' => 'bl1',
+            'leagueSeason'   => '2024',
+            'sport'          => (object) ['sportId' => '1'],
+        ];
+
+        $league = OpenLigaDBLeagueFactory::createFromJSON($json);
+
+        $this->assertSame('1. Bundesliga 2024/2025', $league->getLeagueNameShort());
+    }
+
     public function test_casts_season_and_sport_id_to_int(): void
     {
         $json = (object) [
